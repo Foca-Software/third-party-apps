90acebf62e5ae0c55447c65c9dccab019f0a212b
augusto-foca <acaceres@focasoftware.com>
2026-01-06 13:36:06 -0300

    [MIG] odoo_po_three_levels_approval: Migration to 18.0

--- odoo_po_three_levels_approval/__manifest__.py
+++ odoo_po_three_levels_approval/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'PO Three Level Approval',
-    'version': '1.0.1',
+    'version': "18.0.1.0.0",
     'category': 'Purchases',
     'author': 'FreelancerApps',
     'summary': 'Purchase Order approval : Purchase Manager, Finance Manager, Director Approval multi level approve three level approve purchase order approve purchase order three level approval multi-level approval purchase_order_triple_approval double Approve double Approval Tripple Approve Purchase Tripple Approval Process Sale Order Tripple Approval payslip_tripple_approval Payslip Tripple Approval invoice triple approval sale order triple approval Tripple Approval Sales Quote sale_tripple_approv generate barcode product auto Restrict Read Only User Hide Any Menu Restrict User Menus multi level approve three level approve Tripple Approve Purchase Tripple Approval Project Checklist Task Checklist website document attachment product attachment',
