5232bc14b9ac50499a5dd86c5645635a9bffb5f6
augusto-foca <acaceres@focasoftware.com>
2026-01-06 12:04:04 -0300

    [MIG] account_budget_alert_app: Migration to 18.0

--- account_budget_alert_app/__manifest__.py
+++ account_budget_alert_app/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name' : 'Account Budget Limit Alert-Validation Warning',
     'author': "Edge Technologies",
-    'version': "15.0.1.0.0",
+    'version': "18.0.1.0.0",
     'live_test_url':'https://youtu.be/Q-46v4WzE0M',
     "images":["static/description/main_screenshot.png"],
     'summary' : 'Accounting Budget Limit Alert Budget limit Warning against Purchase budget limit alerts against Bill budget exceed alerts budget limit alert accounting budget validation against purchase budget integration budget warning limit exceed warning on budget',
