226145c2b0e2d8cc7f7f90bb30b7dd57e1822a67
augusto-foca <acaceres@focasoftware.com>
2026-01-06 14:14:45 -0300

    [CHG] om_hr_payroll_account

--- om_hr_payroll_account/models/hr_payroll_account.py
+++ om_hr_payroll_account/models/hr_payroll_account.py
@@ -13,12 +13,12 @@ class HrPayslipLine(models.Model):
         """
         # use partner of salary rule or fallback on employee's address
         register_partner_id = self.salary_rule_id.register_id.partner_id
-        partner_id = register_partner_id.id or self.slip_id.employee_id.address_home_id.id
+        partner_id = register_partner_id.id or self.slip_id.employee_id.work_contact_id.id
         if credit_account:
-            if register_partner_id or self.salary_rule_id.account_credit.internal_type in ('receivable', 'payable'):
+            if register_partner_id or self.salary_rule_id.account_credit.account_type in ('asset_receivable', 'liability_payable'):
                 return partner_id
         else:
-            if register_partner_id or self.salary_rule_id.account_debit.internal_type in ('receivable', 'payable'):
+            if register_partner_id or self.salary_rule_id.account_debit.account_type in ('asset_receivable', 'liability_payable'):
                 return partner_id
         return False
 
@@ -84,7 +84,7 @@ class HrPayslip(models.Model):
                         'date': date,
                         'debit': amount > 0.0 and amount or 0.0,
                         'credit': amount < 0.0 and -amount or 0.0,
-                        'analytic_account_id': line.salary_rule_id.analytic_account_id.id,
+                        'analytic_distribution': line.salary_rule_id.analytic_account_id and {line.salary_rule_id.analytic_account_id.id: 100} or False,
                         'tax_line_id': line.salary_rule_id.account_tax_id.id,
                     })
                     line_ids.append(debit_line)
@@ -99,7 +99,7 @@ class HrPayslip(models.Model):
                         'date': date,
                         'debit': amount < 0.0 and -amount or 0.0,
                         'credit': amount > 0.0 and amount or 0.0,
-                        'analytic_account_id': line.salary_rule_id.analytic_account_id.id,
+                        'analytic_distribution': line.salary_rule_id.analytic_account_id and {line.salary_rule_id.analytic_account_id.id: 100} or False,
                         'tax_line_id': line.salary_rule_id.account_tax_id.id,
                     })
                     line_ids.append(credit_line)
